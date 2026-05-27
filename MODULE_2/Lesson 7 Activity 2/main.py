1a315280cdb2d05ab0bef4cafa7dc627138ff5a7
AdvGodziman-fr <advik9784@gmail.com>
2026-05-27 18:46:58 +0530

    MOD 2 Lesson 1 Act 2 done

---
+++ MODULE_2/Lesson 7 Activity 2/main.py
@@ -0,0 +1,22 @@
+import cv2
+
+image = cv2.imread("MODULE_2\Lesson 7 Activity 1\example.jpg")
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+resized_image = cv2.resize(gray_image, (224, 224))
+
+cv2.imshow("Resized Image in Grayscale", resized_image)
+
+key = cv2.waitKey(0)
+print(key)
+
+if key == ord("s"):
+    cv2.imwrite("MODULE_2/Lesson 7 Activity 2/grayscale_resized_image.jpg", resized_image)
+
+else:
+    print("Image not Saved")
+
+cv2.destroyAllWindows()
+
+print(f"Image Dimensions: {resized_image.shape}")
