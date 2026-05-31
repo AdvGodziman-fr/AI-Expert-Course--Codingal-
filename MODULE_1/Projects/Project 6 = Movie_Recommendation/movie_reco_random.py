d9072bb608c9bd6c4b87adcd738dc82d16ffc63a
AdvGodziman-fr <advik9784@gmail.com>
2026-05-31 12:43:39 +0530

    lesson 8 act 1 done

--- MODULE_1/Projects/Project 6 = Movie_Recommendation/movie_reco_random.py
+++ MODULE_1/Projects/Project 6 = Movie_Recommendation/movie_reco_random.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from colorama import Fore, Style
-from datetime import time
+import time
+import random
 
 i = 0
 
@@ -12,27 +13,62 @@ def load_data(file_path='MODULE_1\imdb_top_1000.csv'):
         print("File Not Found!")
         raise SystemExit
     
+movies_df = load_data()
+    
 
 def animation_time():
     for i in range(0, 3):
-        print(".", end = "")
         time.sleep(1)
+        print(".", end = "", flush=True)
+
 
 def name_check():
     while True:
         username = input(f"{Fore.CYAN}Please enter your name: {Fore.GREEN}").strip()
+        name = username.replace(" ", "")
 
         print(f"{Fore.RESET}Verifying username", end="")
         animation_time()
 
-        if username.isalpha():
+        print()
+        if name.isalpha():
             return username
             
         else:
             print("There has been an error! Please try again!")
             print()
             continue
-            
+
+def recom_random(n=5):
+    d = movies_df
+    suggestions = []
+
+    for i in range(n):
+        movie = random.choice(d["Series_Title"])
+        suggestions.append(movie)
+
+    return suggestions
+
+
+def reco_checker():
+    reco = input(">> ").strip().lower()
+    if "random" in reco: 
+        print("Here are your five recommendations: ")
+        print()
+
+        lst = recom_random()
+
+        for t, f in enumerate(lst, start=1):
+            print(f"{t}. {Fore.YELLOW}{Style.BRIGHT}{f}{Fore.RESET}{Style.RESET_ALL}")
+
+        print()
+        
+    elif "AI" in reco:
+        print("Alright, please wait a few seconds...")
+
+    else:
+        print("Error! Kindly type in 'AI' for AI-based recommendation or 'random' for Random recommendation")            
+
 
 def main_convo():
     print(f"{Fore.LIGHTMAGENTA_EX}Welcome to {Fore.LIGHTGREEN_EX}MOVIE-RECO.com{Fore.LIGHTMAGENTA_EX}, where you can get personalised suggestions regarding movies!{Style.RESET_ALL}")
@@ -40,8 +76,8 @@ def main_convo():
     final_name = name_check()
 
     print(f"{Fore.LIGHTMAGENTA_EX}Greetings, {Fore.LIGHTBLUE_EX}{final_name}{Fore.LIGHTMAGENTA_EX}!{Fore.RESET}")
+    print("Would you like to get a random recommendation or an AI-Powered Recommendation of a movie? ")
 
-    
-
+    reco_checker()
 
 main_convo()
---
+++ MODULE_2/Lesson 8 Activity 1/main.py
@@ -0,0 +1,35 @@
+import cv2
+
+import matplotlib.pyplot as plt
+
+# Reads the image into memory
+image = cv2.imread("MODULE_2/example_img.jpg")
+
+# Convert BGR to RGB
+img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+# Display image data as a plot, interpreting values as colors
+plt.figure(1)
+plt.imshow(img_rgb)
+plt.title("RGB Image")
+
+# Convert to Grayscale
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+plt.figure(2)
+plt.imshow(img_gray, cmap = 'copper')
+plt.title("Grayscale Image")
+
+# INTERNAL OPERATION: Compute a single-channel grayscale image -- GrayPixel = 0.299*R + 0.587*G + 0.114*B
+
+# Cropping the image
+cropped_img = image[100:300, 200:400]
+cropped_rgb = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2RGB)
+
+plt.figure(3)
+plt.imshow(cropped_rgb)
+plt.title("Cropped Image")
+
+
+plt.show()
