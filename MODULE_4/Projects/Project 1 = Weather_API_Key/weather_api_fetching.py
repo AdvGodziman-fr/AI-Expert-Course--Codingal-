c42ba9c4f55865949096a19d209cbbbc767b2f00
AdvGodziman-fr <advik9784@gmail.com>
2026-07-29 20:04:00 +0530

    P1M4 done

---
+++ MODULE_4/Projects/Project 1 = Weather_API_Key/weather_api_fetching.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_weather_info(city_name):
+    # Endpoint
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=B2253497d605813f6b954cf25e598b1a6&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+
+        print(weather_data)
+
+    else:
+        print(response.status_code)
+        print("ERROR: Data is not ready for display!")
+
+
+def main():
+    print("Welcome to Weather Detection AI - YOUR TRUSTED WEATHER DETECTOR")
+
+    while True:
+        user_input = input("Please enter the name of the city to get its weather, or type 'exit' to quit: ")
+
+        if user_input.lower() == "exit":
+            print("Alright! See you later!")
+            break
+        elif not user_input.strip():
+            print("Error! Kindy enter an appropriate city name.")
+        else:
+            get_weather_info(user_input)
+        #print(weather)
+
+
+main()
