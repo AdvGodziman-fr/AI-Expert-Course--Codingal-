880744e16052bcf78f7f87fd9798ae6945e44d29
AdvGodziman-fr <advik9784@gmail.com>
2026-09-20 16:16:46 +0530

    done

--- MODULE_2/Projects/Project 3 = Image_Annotations/image_annotator.py
+++ MODULE_2/Projects/Project 3 = Image_Annotations/image_annotator.py
@@ -13,19 +13,24 @@ img = cv2.imread("MODULE_2\Assets\Images\image.png")
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Getting the width and height of the image
-width, height = img_rgb.shape[0:2]
+height, width = img_rgb.shape[0:2]
 
 # Arrowed Line
-arrow_start = (height - 20 , 20)
-arrow_end = (height - 20, width - 25)
+arrow_start = (width - 20 , 20)
+arrow_end = (width - 20, height - 25)
+ar_hori_start = (10, 20)
+ar_hori_end = (width - 20, 20)
 
-cv2.arrowedLine(img_rgb, arrow_start, arrow_end, COLOR_ORANGE, tipLength=0.02)
-cv2.arrowedLine(img_rgb, arrow_end, arrow_start, COLOR_ORANGE, tipLength=0.02)
+cv2.arrowedLine(img_rgb, arrow_start, arrow_end, COLOR_ORANGE, tipLength=0.02, thickness=2)
+cv2.arrowedLine(img_rgb, arrow_end, arrow_start, COLOR_ORANGE, tipLength=0.02, thickness=2)
+
+cv2.arrowedLine(img_rgb, ar_hori_start, ar_hori_end, COLOR_CYAN, tipLength=0.02, thickness=2)
+cv2.arrowedLine(img_rgb, ar_hori_end, ar_hori_start, COLOR_CYAN, tipLength=0.02, thickness=2)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 height_label_pos = ((arrow_start[0]//2)-125, (arrow_start[1]+arrow_end[1])//2)
 
-cv2.putText(img_rgb, f"Image Width: {width} px; Image Height: {height} px", height_label_pos, font, 0.5, COLOR_CYAN, 1, cv2.LINE_AA)
+cv2.putText(img_rgb, f"Image Width: {width} px; Image Height: {height} px", height_label_pos, font, 0.5, COLOR_CYAN, 2, cv2.LINE_AA)
 
 
 # Figure plotting and displaying
